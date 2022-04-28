da515df8d55b4925d1c0bc5cf7e1f9159d1d9846
mrshukhratbek <shukhratbekqobulov@gmail.com>
2022-04-28 12:25:24 +0500

    feat: create tree

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
 import './App.css';
+import Tree from 'react-d3-tree';
 
 function App() {
-  return <div>App</div>;
+  const orgChart = {
+    name: 'CEO',
+    children: [
+      {
+        name: 'Manager',
+        attributes: {
+          department: 'Production',
+        },
+        children: [
+          {
+            name: 'Foreman',
+            attributes: {
+              department: 'Fabrication',
+            },
+            children: [
+              {
+                name: 'Worker',
+              },
+            ],
+          },
+          {
+            name: 'Foreman',
+            attributes: {
+              department: 'Assembly',
+            },
+            children: [
+              {
+                name: 'Worker',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Tree data={orgChart} />
+    </div>
+  );
 }
 
 export default App;
