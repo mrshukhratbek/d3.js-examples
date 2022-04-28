f94fd1921ddce03d94a700c708215ad29dc83bed
mrshukhratbek <shukhratbekqobulov@gmail.com>
2022-04-29 03:13:48 +0500

    fix: change some errors

--- src/app.props.js
+++ src/app.props.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useCenteredTree } from './utils';
 
 export const useAppProps = () => {
--- src/utils/hoc.js
+++ src/utils/hoc.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const hoc = (hook, Component, displayName = 'HocWrapper') => {
   Component.displayName = displayName;
 
