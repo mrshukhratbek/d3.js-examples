4406f6cba38c3878bb670aded0d93fff9011ced6
mrshukhratbek <shukhratbekqobulov@gmail.com>
2022-04-29 10:43:52 +0500

    ibi

--- src/app.component.js
+++ src/app.component.js
@@ -1,5 +1,49 @@
 import './app.css';
+import React from 'react';
+import { Graph } from 'react-d3-graph';
+
+// graph payload (with minimalist structure)
+const data = {
+  nodes: [{ id: 'Harry' }, { id: 'Sally' }, { id: 'Alice' }],
+  links: [
+    { source: 'Harry', target: 'Sally' },
+    { source: 'Harry', target: 'Alice' },
+  ],
+};
+
+// the graph configuration, just override the ones you need
+const myConfig = {
+  nodeHighlightBehavior: true,
+  node: {
+    color: 'lightgreen',
+    size: 120,
+    highlightStrokeColor: 'blue',
+  },
+  link: {
+    highlightColor: 'lightblue',
+  },
+};
+
+const onClickNode = function(nodeId) {
+  window.alert(`Clicked node ${nodeId}`);
+};
+
+const onClickLink = function(source, target) {
+  window.alert(`Clicked link between ${source} and ${target}`);
+};
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <Graph
+        id="graph-id" // id is mandatory
+        data={data}
+        config={myConfig}
+        onClickNode={onClickNode}
+        onClickLink={onClickLink}
+      />
+      ;
+    </div>
+  );
 };
