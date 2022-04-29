2420a4ed9376a76b86f991a1364ec4b54865c338
mrshukhratbek <shukhratbekqobulov@gmail.com>
2022-04-29 15:08:30 +0500

    feat: change data

--- src/app.props.js
+++ src/app.props.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useCenteredTree } from './utils';
 
 export const useAppProps = () => {
--- src/data/data.js
+++ src/data/data.js
@@ -48,6 +48,60 @@ export const data = [
           subtitle: 'Card subtitle',
           text: 'Some text to build on the card.',
         },
+        children: [
+          {
+            name: 'child2',
+            attributes: {
+              title: 'child2',
+              subtitle: 'Card subtitle',
+              text: 'Some text to build on the card.',
+            },
+            children: [
+              {
+                name: 'child2',
+                attributes: {
+                  title: 'child2',
+                  subtitle: 'Card subtitle',
+                  text: 'Some text to build on the card.',
+                },
+                children: [
+                  {
+                    name: 'child2',
+                    attributes: {
+                      title: 'child2',
+                      subtitle: 'Card subtitle',
+                      text: 'Some text to build on the card.',
+                    },
+                  },
+                  {
+                    name: 'child2',
+                    attributes: {
+                      title: 'child3',
+                      subtitle: 'Card subtitle',
+                      text: 'Some text to build on the card.',
+                    },
+                  },
+                ],
+              },
+              {
+                name: 'child2',
+                attributes: {
+                  title: 'child3',
+                  subtitle: 'Card subtitle',
+                  text: 'Some text to build on the card.',
+                },
+              },
+            ],
+          },
+          {
+            name: 'child2',
+            attributes: {
+              title: 'child3',
+              subtitle: 'Card subtitle',
+              text: 'Some text to build on the card.',
+            },
+          },
+        ],
       },
     ],
   },
