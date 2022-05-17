333de4f07e57fef361244d74ac4d7c23fb25ace0
shukhratbek <skobulov@uzassystem.uz>
2022-05-17 04:24:19 -0700

    feat: add buttons

--- src/components/tree-node/tree-node.component.js
+++ src/components/tree-node/tree-node.component.js
@@ -1,6 +1,10 @@
+// @styles
+import "./tree-node.css";
 // @react
 import React from "react";
+// @utils
 import { hoc } from "../../utils";
+// @hook
 import { useTreeNode } from "./tree-node.props";
 
 export const TreeNode = hoc(
@@ -20,11 +24,15 @@ export const TreeNode = hoc(
               }}
             >
               <h3 style={{ textAlign: "center" }}>{nodeDatum.name}</h3>
-              {nodeDatum.children && (
-                <button style={{ width: "100%" }} onClick={toggleNode}>
-                  {nodeDatum.__rd3t.collapsed ? "Open" : "Close"}
-                </button>
-              )}
+              <div className="button-wrapper">
+                {nodeDatum.children && (
+                  <button onClick={toggleNode}>
+                    {nodeDatum.__rd3t.collapsed ? "Open" : "Close"}
+                  </button>
+                )}
+                <button>Edit</button>
+                <button>Add</button>
+              </div>
             </div>
           </foreignObject>
         </g>
