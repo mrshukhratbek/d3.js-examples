99cb90bceb5fbf4c2a6bac1c75c0b47574125fdf
shukhratbek <skobulov@uzassystem.uz>
2022-05-20 03:37:51 -0700

    feat: add eslint

--- src/contexts/values.js
+++ src/contexts/values.js
@@ -24,6 +24,7 @@ export const ValuesProvider = ({ children }) => {
       data,
       nodeArr: [...new Set(createdArray)],
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
 
   return (
